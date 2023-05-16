a7c2cc4e55222efe6f0aeb97440ec3c1c7e4460f
guilh <guilhermesoares278@gmail.com>
2023-05-16 14:37:18 -0300

    aula 4 exer 6 (evolução 3)

---
+++ aula 4 exercício 6 (evolução 3)/aula4exer6Evolucao3_Apaga_GuilhermeRocha_211039789.sql
@@ -0,0 +1,18 @@
+drop table VEICULO;
+
+drop table PROPRIETARIO;
+
+drop table telefone;
+
+drop table MODELO;
+
+drop table CATEGORIA;
+
+drop table TIPOINFRACAO;
+
+drop table AGENTE;
+
+drop table LOCAL;
+
+drop table INFRACAO
+
